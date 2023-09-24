f53e2033ce7b4e64eeddcacc33d9b6ec8daff5e1
devjwsong <devjwsong@gmail.com>
2023-09-23 20:50:41 -0400

    Implemented the basic player character class.

--- src/agents/player.py
+++ src/agents/player.py
@@ -1,32 +1,84 @@
 # The player class.
 class Player():
-    def __init__(self, **init_params):
-        # Basic specs.
-        self.hp = init_params['hp']
-        self.actions = init_params['actions']
-        self.items = init_params['items']
+    def __init__(self, **kwargs):
+        # Player character info based on the sheet in the text book.
+        self.name = kwargs['name']
+        self.kin = kwargs['kin']
+        self.persona = kwargs['persona']
+        self.goal = kwargs['goal']
+
+        self.traits = kwargs['traits']
+        self.flaws = kwargs['flaws']
+        self.items = kwargs['items']
+
+    # Printing the character sheet so far.
+    def show_info(self):
+        print("#" * 50)
+        print(f"<Character Sheet: {self.name}>")
+        print(f"Name: {self.name}")
+        print(f"Kin: {self.kin}")
+        
+        print("Persona")
+        for s, sent in enumerate(self.persona):
+            print(f"{s+1}. {sent}")
+
+        print(f"Goal: {self.goal}")
+
+        print("Traits")
+        for s, sent in enumerate(self.traits):
+            print(f"{s+1}. {sent}")
+
+        print("Flaws")
+        for s, sent in enumerate(self.flaws):
+            print(f"{s+1}. {sent}")
+
+        print("Items")
+        for i, item in enumerate(self.items):
+            print(f"{i+1}. {item['name']}: {item['description']}")
+
+    # Confirming the modification.
+    def confirm(self, desc):
+        while True:
+            res = input(f"{desc} ('y' or 'n')")
+            if res == 'y':
+                return True
+            elif res == 'n':
+                return False
+
+            print("Your answer should be either 'y' or 'n'.")
+            
+    # Adding trait.
+    def add_trait(self, trait):
+        confirmed = self.confirm(desc=f"Are you sure to add this trait: {trait}?")
+        if confirmed:
+            self.traits.append(trait)
     
-        # Additional context.
-        self.name = init_params['name']
-        self.setting = init_params['setting']
-        self.persona = init_params['persona']
-        self.goal = init_params['goal']
+    # Adding flaw.
+    def add_flaw(self, flaw):
+        confirmed = self.confirm(desc=f"Are you sure to add this flaw: {flaw}?")
+        if confirmed:
+            self.flaws.append(flaw)
+    
+    # Adding item.
+    def add_item(self, item_name, item_desc):
+        confirmed = self.confirm(desc=f"Are you sure to add this item: ({item_name}: {item_desc})?")
+        if confirmed:
+            self.items.append({'name': item_name, 'description': item_desc})
 
-    # Adding a query.
-    def make_query(self):
-        query = input(f"{self.name}: ")
-        return query
+    # Removing trait.
+    def remove_trait(self, idx):
+        confirmed = self.confirm(desc=f"Are you sure to remove this trait: {self.traits[idx]}?")
+        if confirmed:
+            self.traits = self.traits[:idx] + self.traits[idx+1:]
 
-    # Listing the available actions.
-    def list_actions(self):
-        print("<Available actions and damages>")
-        for idx, pair in self.actions.items():
-            print(f"{idx}: {pair[0]} - {pair[1]}.")
+    # Removing flaw.
+    def remove_flaw(self, idx):
+        confirmed = self.confirm(desc=f"Are you sure to remove this flaw: {self.flaws[idx]}?")
+        if confirmed:
+            self.flaws = self.flaws[:idx] + self.flaws[idx+1:]
 
-    # Listing the available items.
-    def list_items(self):
-        print("<Available items with name, type and description>")
-        for idx, triple in self.items.items():
-            print(f"{idx}: {triple[0]}")
-            print(f"type: {triple[1]}")
-            print(f"desc: {triple[2]}")
+    # Removing item.
+    def remove_item(self, idx):
+        confirmed = self.confirm(desc=f"Are you sure to remove this item: ({self.items[idx]['name']}, {self.items[idx]['description']})?")
+        if confirmed:
+            self.items = self.items[:idx] + self.items[idx+1:]
